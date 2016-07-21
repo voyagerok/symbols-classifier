ddc5582d668559e675728a485b0bc6562918460a
Николай <nikolay@localhost.localdomain>
2016-07-21 23:39:26 +0300

    Some minor changes

--- src/probability.c
+++ src/probability.c
@@ -6,7 +6,7 @@ flip_coin (double probability)
 {
   double rand_val;
 
-  rand_val = (double)(rand() % 10000 + 1) / 10000;
+  rand_val = (double)(rand() % 100000 + 1) / 100000;
 
   return rand_val <= probability;
 }
